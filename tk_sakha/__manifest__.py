df86eccecadff79439b3a820b99d2fc6b3ec64fc
butirpadi <butirpadi@gmail.com>
2018-11-17 14:38:12 +0700

    Add Module TK Sakha

---
+++ tk_sakha/__manifest__.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "TK Sakha",
+
+    'summary': """
+        Custom set for TK Sakha""",
+
+    'description': """
+        Custom set for TK Sakha
+    """,
+
+    'author': "Tepat Guna Karya",
+    'website': "http://www.tepatguna.id",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/flectra/flectra/blob/master/flectra/addons/base/module/module_data.xml
+    # for the full list
+    'category': 'Education',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['base', 'siswa_pgtk_ocb11'],
+
+    # always loaded
+    'data': [
+        'security/ir.model.access.csv',
+        # 'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        'demo/demo.xml',
+    ],
+}
---
+++ tk_sakha/controllers/controllers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from flectra import http
+
+# class TkSakha(http.Controller):
+#     @http.route('/tk_sakha/tk_sakha/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/tk_sakha/tk_sakha/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('tk_sakha.listing', {
+#             'root': '/tk_sakha/tk_sakha',
+#             'objects': http.request.env['tk_sakha.tk_sakha'].search([]),
+#         })
+
+#     @http.route('/tk_sakha/tk_sakha/objects/<model("tk_sakha.tk_sakha"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('tk_sakha.object', {
+#             'object': obj
+#         })
