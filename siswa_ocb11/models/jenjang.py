7b56ec36f1ff84cb0b8ceee608df1ef55fe1340a
butirpadi <butirpadi@gmail.com>
2018-11-18 08:17:51 +0700

    Add sort_ordder field to jenjang

--- siswa_ocb11/models/jenjang.py
+++ siswa_ocb11/models/jenjang.py
@@ -3,22 +3,28 @@
 from odoo import models, fields, api
 from pprint import pprint
 
+
 class jenjang(models.Model):
     _name = 'siswa_ocb11.jenjang'
 
     name = fields.Char(string="Nama", required=True)
-    order = fields.Integer('Order')
+    sort_order = fields.Integer('Order')
     desc = fields.Char(string="Keterangan")
     
     @api.model
     def create(self, vals):        
+        # get max sort_order
+        self.env.cr.execute('select max(sort_order) from siswa_ocb11_jenjang')
+        max_order = self.env.cr.fetchone()
+        vals['sort_order'] = max_order[0] + 1
+        
         result = super(jenjang, self).create(vals)
         
         print('--------------------------------------------------------------------------')
         print('Auto generating tahunajaran jenjang for jenjang : ' + result.name)
         print('--------------------------------------------------------------------------')
         # auto generate biaya_registrasi on jenjang create
-        tahunajaran_ids = self.env['siswa_ocb11.tahunajaran'].search([('id', 'ilike', '%'),('active', 'ilike', '%')])
+        tahunajaran_ids = self.env['siswa_ocb11.tahunajaran'].search([('id', 'ilike', '%'), ('active', 'ilike', '%')])
         pprint(tahunajaran_ids)
         for ta in tahunajaran_ids:
             # get biaya_registrasi with jenjang
@@ -33,3 +39,51 @@ class jenjang(models.Model):
                 })
         
         return result    
+    
+    def shift_up(self):
+        if self.sort_order > 1:
+            # get prev item
+            ta_prev = self.env['siswa_ocb11.jenjang'].search([
+                ('sort_order', '=', self.sort_order - 1)
+            ])
+
+            # update prev sort_order
+            ta_prev.write({
+                'sort_order' : self.sort_order
+            })
+
+            # update mine sort_order
+            self.write({
+                'sort_order' : self.sort_order - 1
+            })
+    
+    def shift_down(self):
+        self.env.cr.execute('select max(sort_order) from siswa_ocb11_jenjang')
+        max_sort_order = self.env.cr.fetchone()            
+        if self.sort_order < max_sort_order[0]:
+            # get next item
+            ta_next = self.env['siswa_ocb11.jenjang'].search([
+                ('sort_order', '=', self.sort_order + 1)
+            ])
+
+            # update prev sort_order
+            ta_next.write({
+                'sort_order' : self.sort_order
+            })
+
+            # update mine sort_order
+            self.write({
+                'sort_order' : self.sort_order + 1
+            })
+    
+    @api.multi
+    def unlink(self):
+        for rec in self:
+            # re order other data yang sort_order nya lebih besar dari ini
+            my_sort_order = rec.sort_order 
+            jenjang_ids = self.env['siswa_ocb11.jenjang'].search([('sort_order','>',my_sort_order)])
+            
+            for jj in jenjang_ids:
+                jj.sort_order = jj.sort_order- 1
+                 
+        return super(jenjang, self).unlink()
