9dc3929133ec6894aa88305235c25900e18752f2
Butirpadi <butirpadi@gmail.com>
2018-11-28 18:17:47 +0700

    Add module enable multi company

---
+++ siswa_multi/controllers/controllers.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+from flectra import http
+
+# class SiswaMulti(http.Controller):
+#     @http.route('/siswa_multi/siswa_multi/', auth='public')
+#     def index(self, **kw):
+#         return "Hello, world"
+
+#     @http.route('/siswa_multi/siswa_multi/objects/', auth='public')
+#     def list(self, **kw):
+#         return http.request.render('siswa_multi.listing', {
+#             'root': '/siswa_multi/siswa_multi',
+#             'objects': http.request.env['siswa_multi.siswa_multi'].search([]),
+#         })
+
+#     @http.route('/siswa_multi/siswa_multi/objects/<model("siswa_multi.siswa_multi"):obj>/', auth='public')
+#     def object(self, obj, **kw):
+#         return http.request.render('siswa_multi.object', {
+#             'object': obj
+#         })
