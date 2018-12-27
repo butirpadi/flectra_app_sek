eb9b20e7cd18568d516cfbb12f50b9580d8adb8c
Butirpadi <butirpadi@gmail.com>
2018-12-27 07:10:54 +0700

    Update Surat Module

--- siswa_letter/models/__init__.py
+++ siswa_letter/models/__init__.py
@@ -3,3 +3,5 @@
 from . import surat
 from . import surat_tagihan_biaya_rel
 from . import wizard_surat_tagihan
+from . import wizard_surat_tagihan_biaya_rel
+from . import surat_tagihan_siswa_rel
--- siswa_letter/models/surat.py
+++ siswa_letter/models/surat.py
@@ -6,7 +6,8 @@ from odoo import models, fields, api
 class SiswaSurat(models.Model):
     _name = 'siswa.surat'
 
-    name = fields.Char('Name', required=True)
+    name = fields.Char('Name', required=True,
+                       default="SURAT TAGIHAN CONFIGURATION")
     title = fields.Char('Title', required=True)
     perihal = fields.Char('Perihal', required=True, default="")
     content_1 = fields.Html('Content 1')
@@ -20,3 +21,6 @@ class SiswaSurat(models.Model):
         comodel_name='surat.tagihan.biaya.rel',
         inverse_name='surat_id',
     )
+    surat_tagihan_seq = fields.Integer('Sequence', default=1)
+    surat_tagihan_prefix = fields.Char(
+        'Prefix', default="ST", required=True)
