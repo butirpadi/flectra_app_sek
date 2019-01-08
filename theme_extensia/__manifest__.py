49aba20e704d841d13769251affbdb4e623db72b
Butirpadi <butirpadi@gmail.com>
2019-01-08 16:23:15 +0700

    add btn-flat

---
+++ theme_extensia/__manifest__.py
@@ -0,0 +1,23 @@
+{
+    "name": "Extensia",
+    "description": "A custom theme",
+    "version": "0.1",
+    "author": "Senthilnathan G",
+    "website":"http://maxvuetech.com",
+    "category": "Theme/Backend",
+    "data": [
+        "views/assets.xml",
+        "views/web.xml"
+    ],
+    "images":[
+        "images/screen.png"
+    ],
+    "depends": [
+        "web"
+    ],
+    "auto_install": False,
+    "installable": True,
+    "qweb": [
+        "static/src/xml/web.xml",
+    ],
+}
